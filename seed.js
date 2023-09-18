a05ffa5898f7ed3adf1b55fc5c37c8f32381409d
liamjones2 <liammjoness@gmail.com>
2023-09-18 13:54:59 +0100

    Fixed connection and seeding

---
+++ seed.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require("mongodb");
+const {password} = require("./db_password")
+const uri =
+
+  `mongodb+srv://emm__:Q6g5KV2L1SDZy6kb@cluster0.pfbhecj.mongodb.net`;
+
+const client = new MongoClient(uri);
+const dbConnection = (req, res) => {
+  return client.connect()
+};
+
+const closeConnection = (req, res) => {
+    return client.close()
+}
+
+// you will need to create your own db_password.js function
+// make and export a function that just returns the password provided seperate to the repo
+// if you do not call the file db_password then ensure it is still added to .git.ignore
+// you will still need to connect to the db with mongoDB extention or in terminal to get up and running
+
+const testSeed = (data) => {
+    console.log(data)
+    dbConnection()
+    .then(() => {
+        const db = client.db("testDb")
+        return db.collection("users").drop()
+        
+    })
+    .then(() => {
+        const db = client.db("testDb")
+        return db.collection("users").insertMany(data)
+    })
+}
+
+module.exports = {testSeed, closeConnection}
