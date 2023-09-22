930e3a461bedc162e17acf26462b8a7f33eb30ba
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-09-22 17:30:24 +0100

    removed old mongo project references

--- connection.js
+++ connection.js
@@ -3,25 +3,16 @@ const { password } = require("./db_password")
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "test";
+
 require("dotenv").config({ path: `${__dirname}/.env.${ENV}` });
 
-
-let uri
-if (ENV === "test"){
-   uri = `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`
-}  
-
-else if (ENV === "live") {
-  uri = `mongodb+srv://Emm:k89J6N7JN522M3Q3@cluster0.pdcei6g.mongodb.net/`;
-}
+let uri = `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`
 
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
 
-
 const client = new MongoClient(uri);
-
 
 const dbConnection = (req, res) => {
   return client.connect()
@@ -33,4 +24,5 @@ const dbConnection = (req, res) => {
 
 dbConnection()
 const pool = new Pool()
+
 module.exports = {ENV, pool, client};
--- seed.js
+++ seed.js
@@ -2,8 +2,8 @@ const { MongoClient } = require("mongodb");
 const { password } = require("./db_password")
 
 const uri =
+    
     `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`;
-//   `mongodb+srv://Emm:k89J6N7JN522M3Q3@cluster0.pdcei6g.mongodb.net/`;
 
 const client = new MongoClient(uri);
 const dbConnection = (req, res) => {
@@ -13,11 +13,6 @@ const dbConnection = (req, res) => {
 const closeConnection = (req, res) => {
     return client.close()
 }
-
-// you will need to create your own db_password.js function
-// make and export a function that just returns the password provided seperate to the repo
-// if you do not call the file db_password then ensure it is still added to .git.ignore
-// you will still need to connect to the db with mongoDB extention or in terminal to get up and running
 
 const testSeed = (data) => {
     const db = client.db('game-master-live')
