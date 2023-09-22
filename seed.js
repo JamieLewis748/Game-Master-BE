d6090b25c482ab0b5deb3e5c57401545a3bf8587
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-09-22 16:47:36 +0100

    always on project game-master in mongo

--- seed.js
+++ seed.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require("mongodb");
 const {password} = require("./db_password")
 const uri =
-  //   `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`;
-  `mongodb+srv://Emm:k89J6N7JN522M3Q3@cluster0.pdcei6g.mongodb.net/`;
+    `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`;
+//   `mongodb+srv://Emm:k89J6N7JN522M3Q3@cluster0.pdcei6g.mongodb.net/`;
 
 const client = new MongoClient(uri);
 const dbConnection = (req, res) => {
