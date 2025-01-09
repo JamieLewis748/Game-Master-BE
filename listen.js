6d17370c1730206e3f0f2c2f4a6a360a07f30c0f
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2025-01-09 12:31:59 +0000

    remove conditional statement from listen.js as it prevented build

--- listen.js
+++ listen.js
@@ -1,10 +1,8 @@
 const {app} = require("./app")
 
-const ENV = process.env.NODE_ENV || "test"; 
-if (ENV === "test") {
   const server = app.listen(9095, () =>
     console.log("App listening on port 9095!")
   );
-}
+
 
 module.exports = {server};
