669953864f052ada79a8a3106471808b3d449586
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2025-01-09 11:51:32 +0000

    add changed username in uri mongodb connection string from 'emm__' to 'Emm'

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ const ENV = process.env.NODE_ENV || "test";
 require("dotenv").config({ path: `${__dirname}/.env.${ENV}` });
 
 // let uri = `mongodb+srv://emm__:${password}@cluster0.pfbhecj.mongodb.net`
-let uri = `mongodb+srv://emm__:${password}@cluster0.pdcei6g.mongodb.net`;
+let uri = `mongodb+srv://Emm:${password}@cluster0.pdcei6g.mongodb.net`;
 
 const client = new MongoClient(uri);
 
